900ef410b567e87da7b2e5c29a58fe490ce66e7f
kawsikan <kawsinote@gmail.com>
2020-10-27 14:32:51 +0530

    Netflix build complete

--- src/container/browser.js
+++ src/container/browser.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
+import { FooterContainer } from './footer';
 import { FirebaseContext } from '../context/firebase';
-import { Card, Header, Loading } from '../components';
+import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
@@ -27,6 +29,19 @@ export function BrowseContainer({ slides }) {
         setSlideRows(slides[category]);
     }, [slides, category]);
 
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre'],
+        });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [searchTerm]);
+
+
     return profile.displayName ? (
 
         <>
@@ -39,8 +54,8 @@ export function BrowseContainer({ slides }) {
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                        <Header.TextLink>Series</Header.TextLink>
-                        <Header.TextLink>Films</Header.TextLink>
+                        <Header.TextLink active={category === 'series' ? 'true' : 'fasle'} onClick={() => setCategory('series')}>Series</Header.TextLink>
+                        <Header.TextLink active={category === 'films' ? 'true' : 'fasle'} onClick={() => setCategory('films')}>Films</Header.TextLink>
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
@@ -70,8 +85,30 @@ export function BrowseContainer({ slides }) {
             </Header>
 
             <Card.Group>
-
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`} >
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <Player>
+                                <Player.Button />
+                                <Player.Video src="/videos/bunny.mp4" />
+                            </Player>
+                        </Card.Feature>
+                    </Card>
+                ))}
             </Card.Group>
+            <FooterContainer />
         </>
     ) : (
             < SelectProfileContainer user={user} setProfile={setProfile} />
